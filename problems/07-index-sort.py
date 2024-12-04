31b27f858bb35aec6eb4400ac42587ecd84fa8cf
daniel watson <ozzloy@each.do>
2024-12-04 12:06:58 -0800

    sort by second item

--- problems/07-index-sort.py
+++ problems/07-index-sort.py
@@ -1,7 +1,17 @@
-# Create a function that returns a list of tuples sorted by the value of the second index in the tuple.
+# Create a function that returns a list of tuples sorted by the value
+# of the second index in the tuple.
+
 
 # Write your function here.
+def index_sort(tuples):
+    return sorted(tuples, key=lambda x: x[1])
 
-print(index_sort([(1, 2, 3), (6, 8, 9), (0, 5, 0), (2, 0, 4)])) #> [(2, 0, 4), (1, 2, 3), (0, 5, 0), (6, 8, 9)]
-print(index_sort([(9, 55, 11), (7, 14, 5), (32, 41, 12), (8, 5, 2)])) #> [(8, 5, 2), (7, 14, 5), (32, 41, 12), (9, 55, 11)]
-print(index_sort([(0, 9, 1), (4, 3, 0), (6, 5, 14), (64, 32, 28)])) #> [(4, 3, 0), (6, 5, 14), (0, 9, 1), (64, 32, 28)]
+
+# > [(2, 0, 4), (1, 2, 3), (0, 5, 0), (6, 8, 9)]
+print(index_sort([(1, 2, 3), (6, 8, 9), (0, 5, 0), (2, 0, 4)]))
+
+# > [(8, 5, 2), (7, 14, 5), (32, 41, 12), (9, 55, 11)]
+print(index_sort([(9, 55, 11), (7, 14, 5), (32, 41, 12), (8, 5, 2)]))
+
+# > [(4, 3, 0), (6, 5, 14), (0, 9, 1), (64, 32, 28)]
+print(index_sort([(0, 9, 1), (4, 3, 0), (6, 5, 14), (64, 32, 28)]))
