96cced684b22c3d2a343eb0123dc102a190aa124
Katie Leong <155786253+heykatie@users.noreply.github.com>
2024-12-04 11:01:14 -0800

    finished up to 05-big-words - Files: problems/04-concat-tuple.py problems/05-big-words.py

--- problems/05-big-words.py
+++ problems/05-big-words.py
@@ -3,6 +3,18 @@
 
 # Write your function here.
 
-print(big_words(('earth', 'jupiter', 'mars', 'neptune'))) #> ()
-print(big_words(('wakanda', 'melbourne', 'london', 'france'))) #> ('melbourne',)
-print(big_words(('app', 'academy', 'app academy', 'xylophone'))) #> ('app academy', 'xylophone')
+def big_words(words):
+    lst = []
+    for word in words:
+        if len(word) > 8:
+            lst.append(word)
+    return tuple(lst)
+
+# print(tuple((x * 3 for x in (1, 2, 3))))
+
+def big_words2(words):
+    return tuple([word + 'ly' for word in words if len(word) > 8])
+
+# print(big_words(('earth', 'jupiter', 'mars', 'neptune'))) #> ()
+print(big_words2(('wakanda', 'melbourne', 'london', 'france'))) #> ('melbourne',)
+# print(big_words(('app', 'academy', 'app academy', 'xylophone'))) #> ('app academy', 'xylophone')
